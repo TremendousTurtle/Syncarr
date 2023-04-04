699da513ae361be19e6ea770dd789b32cc08be8f
Chris King <cking91977@gmail.com>
2023-04-03 22:42:24 -0700

    fix small copy errors add test event emitter

--- flaskarr/home.py
+++ flaskarr/home.py
@@ -2,8 +2,8 @@ from flask import (Blueprint, current_app, render_template, request)
 import os
 
 from flaskarr.extensions import db, socketio
-#from flaskarr.models import Card
-#from flaskarr.tasks import process_bulk_data
+from flaskarr.models import File, Release
+from flaskarr.tasks import example_task, emit_test_task_event
 
 from celery.result import AsyncResult
 from celery import chain
@@ -13,7 +13,11 @@ bp = Blueprint('home', __name__)
 
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template('home/index.html')
+    return render_template('index.html')
+
+@bp.route('/settings', methods=['GET'])
+def settings():
+    return render_template('settings.html')
 
 @bp.route('/test_socketio', methods=['GET'])
 def test_socketio():
--- flaskarr/static/base.js
+++ flaskarr/static/base.js
@@ -10,3 +10,30 @@ const socket = io("http://192.168.1.234:5050", {
 socket.on('connect', function () {
     return true;
 });
+
+/**************************************************************************
+** Emitter Handler **
+***************************************************************************
+    Any element with class 'emitter' will emit the event specified in the
+    element's 'data-cs-emit' attribute. If the element also has the class
+    'emitter-cmd' then a command event will be emitted instead with the
+    command specified in the 'data-cs-emit' attribute.
+**************************************************************************/
+$('.emitter').on('click', (ev) => {
+    const evElement = $(ev.currentTarget);
+    const data = evElement.attr('data-cs-emit');
+    if (typeof data === 'undefined') {
+        console.error(`'data-cs-emit' undefined on emitter for '${evElement.text()}'`);
+        return false;
+    } else {
+        if (evElement.hasClass('emitter-cmd')) {
+            socket.emit('command', data);
+            console.log(`An emitter sent command: '${data}'`);
+            return false;
+        } else {
+            socket.emit(data);
+            console.log(`An emitter sent event: '${data}'`);
+            return false;
+        }
+    }
+}).preventDefault();
